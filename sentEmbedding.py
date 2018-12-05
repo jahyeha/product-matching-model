a6f790264011e73fbfe2bbf933cc307e9931db6e
Jahye <jahyeha@gmail.com>
2018-12-05 22:10:55 +0900

    Update sentEmbedding.py

--- sentEmbedding.py
+++ sentEmbedding.py
@@ -12,15 +12,22 @@ class SentenceEmbedding:
         self.num_layers = num_layers
         self.bias = bias
         self.bidirectional = bidirectional
-        self.lstm = None
         self.main = Main('data.pickle')
         self.seq_words_vecs = self.main.seq_words_vecs
         self.tensors = self.padding()
 
-    def build(self):
+    def similarity(self, u, v):
+        # Cosine Similarity
+        import numpy as np
+        import math
+        u = np.asarray(list(u))
+        v = np.asarray(list(v))
+        return np.dot(u,v)/(math.sqrt(np.dot(u,u))*math.sqrt(np.dot(v,v)))
+
+    def lstm(self):
         import torch.nn as nn
-        lstm = nn.LSTM(self.input_size, self.hidden_size, self.num_layers, self.bidirectional)
-        output, (hn, cn) = lstm(self.tensors)
+        model = nn.LSTM(self.input_size, self.hidden_size, self.num_layers, self.bidirectional)
+        output, (hn, cn) = model(self.tensors)
         return output
 
     def padding(self):
@@ -28,7 +35,10 @@ class SentenceEmbedding:
         tensors = torch.nn.utils.rnn.pad_sequence(self.seq_words_vecs, batch_first=True)
         return tensors
 
+
+
 if __name__ == '__main__':
     SE = SentenceEmbedding(2, 10, 1) #(input dim, hidden, layer)
-    result = SE.build()
+    result = SE.lstm()
     print(result[0])
+    print(result[0][-1])
